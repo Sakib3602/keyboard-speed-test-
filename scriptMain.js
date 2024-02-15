f7c2715f6f88ec9025f11d29aaccea03f9a273df
Sakib3602 <sakibsarker6969@gmail.com>
2024-02-15 22:46:27 +0600

    update score and life

--- scriptMain.js
+++ scriptMain.js
@@ -54,11 +54,52 @@ function keybordSide(e){
         console.log("got point")
         c()
         removeBg(al1)
+
+        const score1 = document.getElementById("span-score")
+
+        const score = score1.innerText
+        const newScore = parseInt(score)
+        let sum = newScore + 1
+        score1.innerText = sum
+        let pointElement = document.getElementById("point");
+        pointElement.innerText = newScore;
         
     }
     else{
-        console.log("fuck")
+        const life = document.getElementById("life")
+        const life1 = parseInt(life.innerText)
+        console.log(life)
+
+        const makeIt = life1 - 1
+        life.innerText = makeIt
+        if(makeIt === 0){
+            let up = document.getElementById("score-board1")
+            up.classList.remove("hidden")
+           
+            
+
+
+            let up1 = document.getElementById("play-ground")
+
+            up1.classList.add("hidden")
+        }
+        
     }
 }
 
 document.addEventListener("keyup", keybordSide)
+
+function playAgin(){
+
+    let play = document.getElementById("home-screen")
+    play.classList.remove("hidden")
+    let playGround = document.getElementById("play-ground")
+    playGround.classList.add("hidden")
+    let up = document.getElementById("score-board1")
+            up.classList.remove("hidden")
+
+}
+
+
+
+
